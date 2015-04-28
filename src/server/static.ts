55ed9b26d8d65595f9000fe2c9b09a089c12b942
Marko Grešak <marko@gresak.io>
2015-04-28 05:27:21 +0200

    add static server script
    Serve public folder and node_modules:
    - es6-module-loader
    - systemjs
    - traceur
    - angular2
    - rtts_assert
    - normalize.css

---
+++ src/server/static.ts
@@ -0,0 +1,43 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+import * as express from "express";
+import * as path from "path";
+import errorHandler = require("errorhandler");
+
+// Use environment variable `$PORT` or value 3000.
+let port = process.env.PORT || 3000;
+let mode = process.env.NODE_ENV || 'development';
+// Store path to node_modules in root directory.
+let nodeModulesPath = path.join(__dirname, "../../node_modules/");
+// Name of modules which will be served publically.
+let publicNodeModules = [
+  "es6-module-loader",
+  "systemjs",
+  "traceur",
+  "angular2",
+  "rtts_assert",
+  "normalize.css"
+];
+
+let app = express();
+// Statically serve public folder.
+app.use(express.static(path.join(__dirname, '../public')));
+// Statically serve each of specified `publicNodeModules`.
+publicNodeModules.forEach((publicModule) => {
+  app.use("/" + publicModule, express.static(path.join(nodeModulesPath, publicModule)));
+});
+if (mode === 'development') {
+  app.use(errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+}
+else {
+  app.use(errorHandler());
+}
+
+app.listen(port, () => {
+  console.log(`Static server listening on ${port} in ${mode} mode...`)
+});
+
+export var App = app;
