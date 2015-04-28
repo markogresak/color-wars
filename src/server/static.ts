05ccfb0ac7249181c3507a7ae5ff9cb0a6df7d20
Marko Grešak <marko@gresak.io>
2015-04-28 17:08:07 +0200

    ' -> "

--- src/server/static.ts
+++ src/server/static.ts
@@ -6,7 +6,7 @@ import errorHandler = require("errorhandler");
 
 // Use environment variable `$PORT` or value 3000.
 let port = process.env.PORT || 3000;
-let mode = process.env.NODE_ENV || 'development';
+let mode = process.env.NODE_ENV || "development";
 // Store path to node_modules in root directory.
 let nodeModulesPath = path.join(__dirname, "../../node_modules/");
 // Name of modules which will be served publically.
@@ -21,12 +21,12 @@ let publicNodeModules = [
 
 let app = express();
 // Statically serve public folder.
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, " ../public")));
 // Statically serve each of specified `publicNodeModules`.
 publicNodeModules.forEach((publicModule) => {
   app.use("/" + publicModule, express.static(path.join(nodeModulesPath, publicModule)));
 });
-if (mode === 'development') {
+if (mode === "development") {
   app.use(errorHandler({
     dumpExceptions: true,
     showStack: true
