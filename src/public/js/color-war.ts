a2164a03dbac8fd14aeb951cfc1b97c45cb9840a
Marko Grešak <marko@gresak.io>
2015-05-17 03:46:57 +0200

    add Tile class and init it in ColorWar

--- src/public/js/color-war.ts
+++ src/public/js/color-war.ts
@@ -12,14 +12,32 @@ import {Component, View, bootstrap} from "angular2/angular2";
 
 class ColorWar {
 
-  private name: string;
+  private tiles: Array<Array<Tile>>;
 
   constructor() {
-    this.name = "Marko";
+    this.tiles = [];
+
+    for(let i = 0; i < 10; i++) {
+      let tileLine = [];
+      for(let j = 0; j < 10; j++) {
+        tileLine.push(new Tile(i, j));
+      }
+      this.tiles.push(tileLine);
+    }
   }
 
-  updateName(name: string) {
-    this.name = name;
+}
+
+class Tile {
+
+  private x: number;
+  private y: number;
+  private active: boolean;
+
+  constructor(x, y: number) {
+    this.x = x;
+    this.y = y;
+    this.active = false;
   }
 
 }
